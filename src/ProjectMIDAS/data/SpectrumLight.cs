0953deb94d00c9cb6ad9388621bf46ab4c68fd81
Michael Hoopmann <hoopmann@uw.edu>
2024-12-02 16:38:53 -0800

    Setting up basic data structures and tests for memory usage and CPU performance.

---
+++ src/ProjectMIDAS/data/SpectrumLight.cs
@@ -0,0 +1,232 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ProjectMIDAS.Data.SpectrumLight
+{
+    public struct sCentroid
+    {
+        public double Mz { get; set; }
+        public double Intensity { get; set; }
+        public sCentroid(double mz = 0, double intensity = 0) 
+        { 
+            Mz = mz;
+            Intensity = intensity;
+        }
+    }
+
+    /// <summary>
+    /// The struct-based contents of a single spectrum/scan event
+    /// </summary>
+    public class SpectrumLight : IDisposable
+    {
+        /// <summary>
+        /// Constructor for Spectrum class.
+        /// </summary>
+        public SpectrumLight()
+        {
+            //Precursors = new List<Precursor>()
+            //{
+            //    new Precursor()
+            //};
+        }
+
+        /// <summary>
+        /// Current scan number
+        /// </summary>
+        public int ScanNumber { get; set; }
+
+        /// <summary>
+        /// Scan event order
+        /// </summary>
+        public int ScanEvent { get; set; }
+
+        /// <summary>
+        /// The MS level (e.g. 1 = MS1, 2 = MS2, 3 = MS3, MSn)
+        /// </summary>
+        public int MsLevel { get; set; }
+
+        /// <summary>
+        /// Total number of peaks in the current scan
+        /// </summary>
+        public int PeakCount() { return Centroids.Count; }
+
+        /// <summary>
+        /// Thermo variable for master scan number
+        /// </summary>
+        //public int MasterIndex { get; set; }
+
+        /// <summary>
+        /// The current scan description
+        /// </summary>
+        //public string ScanDescription { get; set; }
+
+        /// <summary>
+        /// Injection time used to acquire the scan ions (milliseconds, max = 5000)
+        /// </summary>
+        public double IonInjectionTime { get; set; }
+
+        /// <summary>
+        /// Total time, including injection time, to acquire the current scan (milliseconds)
+        /// </summary>
+        public double ElapsedScanTime { get; set; }
+
+        /// <summary>
+        /// Bool representation of polarity (true = positive)
+        /// </summary>
+        //public Polarity Polarity { get; set; } = Polarity.Positive; //might be overkill to declare a type for this
+        public bool Polarity { get; set; } = true;
+
+        /// <summary>
+        /// Mass analyzer used
+        /// </summary>
+        //public Analyzer Analyzer { get; set; } = Analyzer.IonTrap;
+
+        /// <summary>
+        /// String description of scan type
+        /// </summary>
+        //public string ScanType { get; set; }
+
+        /// <summary>
+        /// Scan filter line from RAW file
+        /// </summary>
+        //public string FilterLine { get; set; }
+
+        /// <summary>
+        /// Scan retention time (minutes)
+        /// </summary>
+        public double RetentionTime { get; set; } = 0;
+
+        /// <summary>
+        /// Mz that scan starts at
+        /// </summary>
+        public double StartMz { get; set; }
+
+        /// <summary>
+        /// Mz that scan ends at
+        /// </summary>
+        public double EndMz { get; set; }
+
+        /// <summary>
+        /// Lowest Mz observed in scan
+        /// </summary>
+        public double LowestMz { get; set; }
+
+        /// <summary>
+        /// Highest Mz observed in scan
+        /// </summary>
+        public double HighestMz { get; set; }
+        /// <summary>
+        /// The most intense Mz peak in the scan
+        /// </summary>
+        public double BasePeakMz { get; set; }
+
+        public double BasePeakIntensity { get; set; }
+
+        /// <summary>
+        /// FAIMS compensation voltage, if used (in volts)
+        /// </summary>
+        public int FaimsCV { get; set; }
+
+        /// <summary>
+        /// FAIMS state, it's possible to have a CV of 0 which is the int default.
+        /// </summary>
+        //public TriState FaimsState { get; set; } = TriState.Off;
+
+        /// <summary>
+        /// Total ion current for the current scan
+        /// </summary>
+        public double TotalIonCurrent { get; set; }
+
+        /// <summary>
+        /// If a dependent scan, the fragmentation energy used
+        /// </summary>
+        public double CollisionEnergy { get; set; }
+
+        /// <summary>
+        /// If a dependent scan, the parent scan number
+        /// </summary>
+        public int PrecursorMasterScanNumber { get; set; }
+
+        /// <summary>
+        /// If a dependent scan, the activation method used to generate the scan fragments
+        /// </summary>
+        //public string PrecursorActivationMethod { get; set; }
+
+        /// <summary>
+        /// The observed centroid peaks in the scan
+        /// </summary>
+        public List<sCentroid> Centroids { get; set; } = new List<sCentroid>();
+
+        /* START TODO: Fill out the rest of this information after speed testing the above code
+
+        /// <summary>
+        /// Holds precursor information for dependent scans.
+        /// There may be multiple precursors for a single scan.
+        /// </summary>
+        /// <typeparam name="Precursor"></typeparam>
+        /// <returns></returns>
+        public List<Precursor> Precursors { get; set; } = new List<Precursor>();
+
+        /// <summary>
+        /// Returns the m/z of the first precursor. 
+        /// </summary>
+        /// <value></value>
+        public double PrecursorMz
+        {
+            get { return Precursors[0].Mz; }
+            set { Precursors[0].Mz = value; }
+        }
+
+        /// <summary>
+        /// Monoisotopic Mz for the precursor, if exist, otherwise should equal PrecursorMz
+        /// </summary>
+        public double MonoisotopicMz { get; set; }
+        /// <summary>
+        /// Returns the charge of the first precursor.
+        /// </summary>
+        /// <value></value>
+        public int PrecursorCharge
+        {
+            get { return Precursors[0].Charge; }
+            set { Precursors[0].Charge = value; }
+        }
+
+        public double PrecursorIntensity
+        {
+            get { return Precursors[0].Intensity; }
+            set { Precursors[0].Intensity = value; }
+        }
+
+        public double IsolationMz
+        {
+            get { return Precursors[0].IsolationMz; }
+            set { Precursors[0].IsolationMz = value; }
+        }
+
+        END TODO */
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Centroids.Clear();
+                    //Centroids = null;
+                }
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+
+}
---
+++ vs/ProjectMIDASApp/PMApp.cs
@@ -0,0 +1,144 @@
+﻿#define GLOBALMEM
+
+using ProjectMIDAS.Data.Peak;
+using ProjectMIDAS.Data.Spectrum;
+using ProjectMIDAS.Data.SpectrumFixed;
+using ProjectMIDAS.Data.SpectrumLight;
+
+class PMApp
+{
+
+    public const int ScanCount= 100000;
+    public const int ScanSize = 5000;
+    public const bool Interrupt = false;
+    public const bool Echo = false;
+
+#if(GLOBALMEM)
+    public List<Spectrum> spec;
+    public List<SpectrumLight> specLight;
+    public List<SpectrumFixed> specFixed;
+#endif
+
+    static void Main(string[] args)
+    {
+        PMApp app = new PMApp();
+
+        app.MemoryTestSpectrumLight(); //Light #1
+        app.Clear();
+        if (Interrupt) app.Pause();
+
+        app.MemoryTestSpectrumFixed(); //Fixed #1
+        app.Clear();
+        if (Interrupt) app.Pause();
+
+        app.MemoryTestSpectrum();  //Normal #1
+        app.Clear();
+        if (Interrupt) app.Pause();
+
+        
+
+        app.MemoryTestSpectrumFixed(); //Fixed #2
+        app.Clear();
+        if (Interrupt) app.Pause();
+
+        app.MemoryTestSpectrum();  //Normal #2
+        app.Clear();
+        if (Interrupt) app.Pause();
+
+        app.MemoryTestSpectrumLight(); //Light #2
+        app.Clear();
+        if (Interrupt) app.Pause();
+
+    }
+
+    PMApp()
+    {
+#if GLOBALMEM
+        spec = new List<Spectrum>();
+        specLight = new List<SpectrumLight>();
+        specFixed = new List<SpectrumFixed>();
+#endif
+    }
+
+    public void Clear()
+    {
+#if GLOBALMEM
+        spec.Clear();
+        specLight.Clear();
+        specFixed.Clear();
+#endif
+    }
+
+    public void MemoryTestSpectrum()
+    {
+        //Create a set of 1000 spectra of 1000 datapoints
+        if(Echo) Console.WriteLine("MemoryTestSpectrum");
+#if (!GLOBALMEM)
+        List<Spectrum> spec = new List<Spectrum>();
+#endif
+        for (int i = 0; i < ScanCount; i++)
+        {
+            Spectrum spectrum = new Spectrum();
+            for(int j = 0; j < ScanSize; j++)
+            {
+                Centroid centroid = new Centroid();
+                centroid.Mz = j;
+                centroid.Intensity = j;
+                spectrum.Centroids.Add(centroid);
+            }
+            spec.Add(spectrum);
+        }
+        if (Echo) Console.WriteLine("MemoryTestSpectrum Finished");
+    }
+
+    public void MemoryTestSpectrumLight()
+    {
+        //Create a set of 1000 spectra of 1000 datapoints
+        if (Echo) Console.WriteLine("MemoryTestSpectrumLight");
+#if (!GLOBALMEM)
+        List<SpectrumLight> specLight = new List<SpectrumLight>();
+#endif
+        for (int i = 0; i < ScanCount; i++)
+        {
+            SpectrumLight spectrum = new SpectrumLight();
+            for (int j = 0; j < ScanSize; j++)
+            {
+                ProjectMIDAS.Data.SpectrumLight.sCentroid centroid = new ProjectMIDAS.Data.SpectrumLight.sCentroid();
+                centroid.Mz = j;
+                centroid.Intensity = j;
+                spectrum.Centroids.Add(centroid);
+            }
+            specLight.Add(spectrum);
+        }
+        if (Echo) Console.WriteLine("MemoryTestSpectrumLight Finished");
+    }
+
+    public void MemoryTestSpectrumFixed()
+    {
+        //Create a set of 1000 spectra of 1000 datapoints
+        if (Echo) Console.WriteLine("MemoryTestSpectrumFixed");
+#if (!GLOBALMEM)
+        List<SpectrumFixed> specFixed = new List<SpectrumFixed>();
+#endif
+        for (int i = 0; i < ScanCount; i++)
+        {
+            SpectrumFixed spectrum = new SpectrumFixed(ScanSize);
+            for (int j = 0; j < ScanSize; j++)
+            {
+                spectrum.Centroids[j].Mz = j;
+                spectrum.Centroids[j].Intensity = j;
+            }
+            specFixed.Add(spectrum);
+        }
+        if (Echo) Console.WriteLine("MemoryTestSpectrumFixed Finished");
+    }
+
+    public void Pause()
+    {
+        Console.WriteLine("Press SPACE to continue");
+        do
+        {
+
+        } while (Console.ReadKey(true).Key != ConsoleKey.Spacebar);
+    }
+}
